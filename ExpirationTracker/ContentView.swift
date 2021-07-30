ce64070cbed1bdf76adc4f83d77ed7db5fcbadf0
Sanjith Udupa <sanjith.udupa@gmail.com>
2021-07-30 15:26:47 -0400

    Initial Commit

---
+++ ExpirationTracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ExpirationTracker
+//
+//  Created by Sanjith Udupa on 7/30/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
